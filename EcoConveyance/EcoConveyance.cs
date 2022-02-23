9801c3158e6d5d23bcdc10a9a506da1a75c2f165
Tavren <tavren.dragon@gmail.com>
2022-02-23 19:00:45 +0300

    Version and change inheritance

--- EcoConveyance/EcoConveyance.cs
+++ EcoConveyance/EcoConveyance.cs
@@ -5,18 +5,13 @@ using Eco.Core.Utils;
 
 namespace Eco.Mods.EcoConveyance
 {
-	public class EcoConveyance : IModKitPlugin, IServerPlugin, IInitializablePlugin
+	public class EcoConveyance : IModInit, IServerPlugin
 	{
 		public const string Version = "1.0.1-alpha";
 
 		public string GetStatus()
 		{
 			return $"{this} v{Version}";
-		}
-
-		public void Initialize(TimedTask timer)
-		{
-
 		}
 
 		public override string ToString()
