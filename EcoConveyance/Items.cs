1eb5afca64cacb5966130a18b70529b9eae84ea3
Tavren <tavren.dragon@gmail.com>
2022-08-31 10:31:43 +0300

    Update Items.cs
    Fix unable to place objects (added RequiresSurfaceOnSides)

--- EcoConveyance/Items.cs
+++ EcoConveyance/Items.cs
@@ -4,6 +4,7 @@ using Eco.Gameplay.Objects;
 using Eco.Gameplay.Players;
 using Eco.Mods.EcoConveyance.Objects;
 using Eco.Shared.Localization;
+using Eco.Shared.Math;
 using Eco.Shared.Serialization;
 using System;
 using System.Collections.Generic;
@@ -45,6 +46,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Hewn Conveyor Line");
 		public override LocString DisplayDescription => Localizer.DoStr("Conveyor used for transporting crates in one direction");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -54,6 +57,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Hewn Conveyor Importer");
 		public override LocString DisplayDescription => Localizer.DoStr("Machine for importing items from connected storage, packing them into crate and sends over conveyor");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -63,6 +68,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Hewn Conveyor Exporter");
 		public override LocString DisplayDescription => Localizer.DoStr("Machine for exporting, unpack items from arrived crates and put them into connected storage");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 	#endregion
 	#region CastIronConveyor
@@ -100,6 +107,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Cast Iron Conveyor Line");
 		public override LocString DisplayDescription => Localizer.DoStr("Conveyor used for transporting crates in one direction");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -109,6 +118,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Cast Iron Conveyor Importer");
 		public override LocString DisplayDescription => Localizer.DoStr("Machine for importing items from connected storage, packing them into crate and sends over conveyor");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -118,6 +129,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Cast Iron Conveyor Exporter");
 		public override LocString DisplayDescription => Localizer.DoStr("Machine for exporting, unpack items from arrived crates and put them into connected storage");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -127,6 +140,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Cast Iron Conveyor Lift");
 		public override LocString DisplayDescription => Localizer.DoStr("Conveyor used for transporting crates up or down, used to transport between floors");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 
 	[Serialized]
@@ -136,6 +151,8 @@ namespace Eco.Mods.EcoConveyance
 	{
 		public override LocString DisplayNamePlural => Localizer.DoStr("Cast Iron Conveyor Lift Adapter");
 		public override LocString DisplayDescription => Localizer.DoStr("Entry and exit point for lift conveyors, put it at the top and bottom of lift conveyors and connect conveyor at the side");
+
+		public override DirectionAxisFlags RequiresSurfaceOnSides { get; } = 0 | DirectionAxisFlags.Down;
 	}
 	#endregion
 }
